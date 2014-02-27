633eae68f295a0f1ac8a4e83c9a4c884ab55d93c
Neem Serra and Zach Becker <ionicmobile+neem.serra+zach.becker@asynchrony.com>
2014-02-27 16:07:50 -0600

    Adding content to the calc.rb file

---
+++ calc.rb
@@ -0,0 +1,29 @@
+puts "How many Hours in a year?"
+x = 24 * 365
+puts x
+puts "How many minutes are in a decade?"
+mins_in_day = 60 * 24
+mins_in_year = mins_in_day * 365
+mins_in_decade = mins_in_year * 10
+puts mins_in_decade
+
+
+
+
+
+
+
+puts "How many seconds old am I?"
+secs_in_day = mins_in_day * 60
+secs_in_year = secs_in_day * 365
+puts secs_in_year * 31
+
+
+
+
+
+
+puts "How old am I if I'm 1031 million seconds old?"
+seconds = 1031 * 1000000
+age = seconds / secs_in_year
+puts age
