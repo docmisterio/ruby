e20b46a7ef7b2d7c380e107893bcf6b213657aea
Zach Becker <doc.misterio@gmail.com>
2014-03-11 21:22:02 -0500

    added a new file as per the home work for chapter 4. mixing it up

---
+++ addNumber.rb
@@ -0,0 +1,5 @@
+puts 'dude, what is your favorite number?'
+favNum = gets.chomp
+newNum = favNum.to_i + 1 
+puts newNum
+# this is currently working, I just need to fix line 4 to be conversational
--- calc.rb
+++ calc.rb
@@ -7,21 +7,10 @@ mins_in_year = mins_in_day * 365
 mins_in_decade = mins_in_year * 10
 puts mins_in_decade
 
-
-
-
-
-
-
 puts "How many seconds old am I?"
 secs_in_day = mins_in_day * 60
 secs_in_year = secs_in_day * 365
 puts secs_in_year * 31
-
-
-
-
-
 
 puts "How old am I if I'm 1031 million seconds old?"
 seconds = 1031 * 1000000
