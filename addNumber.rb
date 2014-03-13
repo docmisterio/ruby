aa106400b28b271bf6ee7c242354bdabd5e5bd6d
Zach Becker <doc.misterio@gmail.com>
2014-03-12 20:54:44 -0500

    adding conversational verbage to code

--- addNumber.rb
+++ addNumber.rb
@@ -1,5 +1,4 @@
 puts 'dude, what is your favorite number?'
 favNum = gets.chomp
 newNum = favNum.to_i + 1 
-puts newNum
-# this is currently working, I just need to fix line 4 to be conversational
+puts 'I don\'t know why you think ' + favNum + ' is the best number because it is clearly ' + newNum.to_s + '.'
