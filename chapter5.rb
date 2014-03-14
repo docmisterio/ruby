5bf90a6e0361fd2b2351d73684d0a998718c8faf
Charlie Sanders <sanderscharlie@gmail.com>
2014-03-13 20:16:15 -0500

    comming the first bit of homework for chapter 5, an angry boss program

--- chapter5.rb
+++ chapter5.rb
@@ -1,13 +1,4 @@
-puts 'What is your first name?'
-firstName = gets.chomp
-puts 'Ok, now your middle and last name, please'
-middleName = gets.chomp
-lastName = gets.chomp
-fullNameCount = firstName + middleName + lastName
-puts 'Thank you... did you know there are ' + fullNameCount.length.to_s + ' characters in your name?'
+puts 'What do you want ya lazy bum?'
 answer = gets.chomp
-if answer == 'no'
-  puts 'I guess you can\'t count, then.'
-else
-  puts 'ok good.'
-end
+puts answer + ' WHAT THE FUCK DO YOU MEAN ' + answer.upcase + '. '
+puts 'Get the fuck out of my office! You\'re fired!'
