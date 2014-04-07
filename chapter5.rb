400a6dfe871dd321318da3179523ef75ed5d2e4e
Zach Becker <doc.misterio@gmail.com>
2014-04-06 19:25:08 -0500

    wrote the 99 bottles of beer program, going to see if I can refactor it

--- chapter5.rb
+++ chapter5.rb
@@ -1,10 +1,16 @@
-command = ''
-
-while command != 'bye'
-  if command == command
-  end
-  command = gets.chomp
-  puts command
+puts 'How many bottles of beer are you starting with?'
+beer = gets.chomp.to_i
+while beer != 2
+  puts beer.to_s + ' bottles of beer on the wall, ' + beer.to_s + ' bottles of beer.'
+  beer = beer - 1
+  puts 'Take one down and pass it around ' + beer.to_s + ' bottles of beer on the wall.'
+  puts
 end
-
-puts 'Come again soon!'
+if beer == 2
+  puts beer.to_s + ' bottles of beer bottles of beer, ' + beer.to_s + ' bottles of beer.'
+  beer = beer - 1
+  puts 'Take one down and pass it around ' + beer.to_s + ' bottle of beer on the wall.'
+end
+puts
+puts 'No more bottles of beer on the wall, no more bottles of beer.'
+puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
