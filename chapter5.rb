cf8ef04835d932bb6c8d5c3d6d56f60f7af3f9ad
Zach Becker <doc.misterio@gmail.com>
2014-04-06 19:31:59 -0500

    99 bottles of beer, refactored

--- chapter5.rb
+++ chapter5.rb
@@ -1,15 +1,20 @@
 puts 'How many bottles of beer are you starting with?'
 beer = gets.chomp.to_i
+firstLine1 = ' bottles of beer on the wall, '
+firstLine2 = ' bottles of beer.'
+secondLine1 = 'Take one down and pass it around '
+secondLine2 = ' bottles of beer on the wall.'
+
 while beer != 2
-  puts beer.to_s + ' bottles of beer on the wall, ' + beer.to_s + ' bottles of beer.'
+  puts beer.to_s + firstLine1  + beer.to_s + firstLine2
   beer = beer - 1
-  puts 'Take one down and pass it around ' + beer.to_s + ' bottles of beer on the wall.'
+  puts secondLine1 + beer.to_s + secondLine2
   puts
 end
 if beer == 2
-  puts beer.to_s + ' bottles of beer bottles of beer, ' + beer.to_s + ' bottles of beer.'
+  puts beer.to_s + firstLine1 + beer.to_s + firstLine2
   beer = beer - 1
-  puts 'Take one down and pass it around ' + beer.to_s + ' bottle of beer on the wall.'
+  puts secondLine1 + beer.to_s + ' bottle of beer on the wall.'
 end
 puts
 puts 'No more bottles of beer on the wall, no more bottles of beer.'
